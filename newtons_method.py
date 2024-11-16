53e00d562afdb2c4ccc9e85050507dadff42b64c
Mason McNeill <masonmcneill@uvic.ca>
2024-11-16 01:16:16 -0800

    Polished up input handling

---
+++ newtons_method.py
@@ -0,0 +1,85 @@
+from sympy import sympify, lambdify, Symbol, diff
+import time
+
+def is_valid_func(input):
+    #Checks if the users input function is a valid math function
+    try:
+        #uses 'eval' so its unsafe but it doesn't matter here.
+        sympify(input)
+    except Exception as e:
+        return False
+    
+def newton(input_function, initial_x, iterations):
+    #Test to see if function is valid
+    test_valid = is_valid_func(input_function)
+    if test_valid is False:
+        print("Function is INVALID, please check your input.")
+        return
+    #Perform function parsing and take the derivative
+    x = Symbol('x')
+    derivative = diff(input_function, x)
+    func = lambdify(x, input_function)
+    deriv_f = lambdify(x, derivative)
+    value = initial_x
+    print("x0 =", value)
+
+    for i in range(iterations):
+        result_y = func(value)
+        result_dy = deriv_f(value)
+        if result_dy == 0:
+            print("Cannot divide by 0, sorry.")
+            break
+        value = value - (result_y / result_dy)
+        index = i+1
+        print(f"x{index} = {value:.5f}", )
+
+def launch_newton():
+    #Newton active while loop for accepting repetitive inputs
+    print("Please input the function.")
+    the_function = input()
+    
+    print("Please input the initial x value.")
+    try:
+        initial_x = float(input())
+    except Exception as e:
+        print("Please make initial x value an integer or decimal.")
+        time.sleep(1)
+        return
+    
+    print("Please input the number of newton iterations.")
+    try:
+        iterations = int(input())
+    except Exception as e:
+        print("Please input number of iterations as positive integer.")
+        time.sleep(1)
+        return
+    
+    print("Performing calculation")
+    time.sleep(1)
+    newton(the_function, initial_x, iterations)
+    time.sleep(2)
+
+#leave while loop, checks user input if they want to do more functions.
+#leave = true means user wants to stop
+def run_program():
+    leave = False
+    while leave is False:
+        leave = False
+        ask_again = True
+        launch_newton()
+        while ask_again:
+            print("Would you like to do another? (Y/N)")
+            response = input()
+            if response.lower() == ("n"):
+                ask_again = False
+                leave = True
+                print("See you later.")              
+            elif response.lower() != ("y"):
+                print("I am lazy I only accept Y or N.")
+                time.sleep(1)
+            else:
+                ask_again = False
+
+run_program()
+   
+            
