057e33e2cd66dd96b28796219b0be1fd12f05e8e
Mason McNeill <masonmcneill@uvic.ca>
2024-11-16 01:47:28 -0800

    Cleaned up, added comments

--- newtons_method.py
+++ newtons_method.py
@@ -43,17 +43,19 @@ def newton(input_function, initial_x, iterations):
         if result_dy == 0:
             print("Cannot divide by 0, sorry.")
             break
+        #Newtons method formula Xn+1 = Xn - [f(Xn)/f'(Xn)]
         value = value - (result_y / result_dy)
         index = i+1
         print(f"x{index} = {value:.5f}", )
 
 def launch_newton():
-    #Newton active while loop for accepting repetitive inputs
+    #take input from user and perform type checks
     print("Please input the function.")
     the_function = input()
     if the_function.lower() == 'help':
         help_menu()
         return
+    
     print("Please input the initial x value.")
     try:
         initial_x = float(input())
@@ -76,7 +78,7 @@ def launch_newton():
     time.sleep(2)
 
 #leave while loop, checks user input if they want to do more functions.
-#leave = true means user wants to stop
+#leave = true means user wants to stop.
 def run_program():
     print("type 'help' for input info")
     leave = False
@@ -102,6 +104,4 @@ def run_program():
             else:
                 ask_again = False
 
-run_program()
-   
-            
+run_program()   
